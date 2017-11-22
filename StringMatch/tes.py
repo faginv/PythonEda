2df4919078c16af431b87b160b205245a61416fc
Fagin Vazio <vaziooizav@gmail.com>
2017-11-22 18:01:10 +1100

    matching string

---
+++ StringMatch/tes.py
@@ -0,0 +1,104 @@
+import csv
+
+def match(X,Y):
+    mismatch = 0
+    for x,y in zip(X,Y):
+            if not (x == 'X' or x == y):
+                mismatch += 1
+
+    #print(mismatch)
+    if mismatch > 1:
+        return False
+    return True
+
+def g4_match(X,Y):
+
+    N = "NTAG"
+    K = "KLQ"
+    D = "DE"
+
+    spl_mismatch, mismatch = (0,0)
+
+    for x,y in zip(X,Y):
+
+        if not (x == 'X' or y == x):
+            if(x == 'T' and y in N):
+                spl_mismatch+=1
+            if(x == 'L' and y in K):
+                spl_mismatch+=1
+            if(x == 'E' and y in D):
+                spl_mismatch+=1
+        # else:
+        #     mismatch+=1
+
+    if mismatch > 0 or spl_mismatch > 2:
+        return False
+
+    return True
+
+#print g4_match(g4,protein)
+
+
+def H(protein,x1,x2,x3,x4):
+
+        def find_matches(x,g4_match):
+            match_positions = []
+            matches         = []
+            for i in range(len(protein) - len(x)):
+                candidate = protein[i : i + len(x)]
+                if match(x, candidate):
+                    match_positions.append(i)
+                    matches        .append(candidate)
+                elif g4_match(x, candidate):
+                    match_positions.append(i)
+                    matches        .append(candidate)
+            print("Mmatches: ",matches, match_positions)
+            return matches, match_positions
+
+        L1, pL1 = find_matches(x1, match)
+        L2, pL2 = find_matches(x2, match)
+        L3, pL3 = find_matches(x3, g4_match)
+        L4, pL4 = find_matches(x4, match)
+        #print L1, pL1
+        #print L2, pL2
+        #print L3, pL3
+        #print L4, pL4
+
+        candidates = []
+        for a in zip(pL1, L1):
+            for b in zip(pL2, L2):
+                for c in zip(pL3, L3):
+                    for d in zip(pL4, L4):
+                        if (40 <= b[0] - a[0] <= 80 and
+                            40 <= c[0] - b[0] <= 80 and
+                            20 <= d[0] - c[0] <= 80    ):
+                            print(a,b,c,d)
+                            candidates.append((a,b,c,d))
+                        elif (80 <= b[0] - a[0] <= 120 and
+                              40 <= c[0] - b[0] <= 80 and
+                              120 <= d[0] - c[0] <= 180 ):
+                            print(a,b,c,d)
+                            candidates.append((a,b,c,d))
+                        elif (40 <= b[0] - a[0] <= 80 and
+                              80 <= c[0] - b[0] <= 80 and
+                              120 <= d[0] - c[0] <= 180 ):
+                            print(a,b,c,d)
+                            candidates.append((a,b,c,d))
+
+
+            with open('output_2.csv', 'a') as myfile:
+                wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+                #wr.writerow([pdb_id])
+                #wr.writerow([protein_name])
+                wr.writerow([protein])
+                #wr.writerow([source])
+                for i in candidates:
+                    wr.writerow([i])
+
+g1 = 'GXXXXGK'
+g3 = 'DXXG'
+g4 = 'NKXD'
+g5 = 'EXSAX'
+
+protein = 'MASEIHMTGPMCLIENTNGRLMANPEALKILSAITQPMVVVAIVGLYRTGKSYLMNKLAGKKKGFSLGSTVQSHTKGIWMWCVPHPKKPGHILVLLDTEGLGDVEKGDNQNDSWIFALAVLLSSTFVYNSIGTINQQAMDQLYYVTELTHRIRSKSSPDENENEVEDSADFVSFFPDFVWTLRDFSLDLEADGQPLTPDEYLTYSLKLKKGTSQKDETFNLPRLCIRKFFPKKKCFVFDRPVHRRKLAQLEKLQDEELDPEFVQQVADFCSYIFSNSKTKTLSGGIQVNGPRLESLVLTYVNAISSGDLPCMENAVLALAQIENSAAVQKAIAHYEQQMGQKVQLPTETLQELLDLHRDSEREAIEVFIRSSFKDVDHLFQKELAAQLEKKRDDFCKQNQEASSDRCSALLQVIFSPLEEEVKAGIYSKPGGYRLFVQKLQDLKKKYYEEPRKGIQAEEILQTYLKSKESMTDAILQTDQTLTEKEKEIEVERVKAESAQASAKMLQEMQRKNEQMMEQKERSYQEHLKQLTEKMENDRVQLLKEQERTLALKLQEQEQLLKEGFQKESRIMKNEIQDLQTKMRRRKACTIS'
+H(protein,g1,g3,g4,g5)
